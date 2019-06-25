80bec52444afdced0143183b7ccd095f59b94f9a
convan <51078084+convan@users.noreply.github.com>
2019-06-25 20:15:02 +0300

    wd
    wd

--- ReflObject.php
+++ ReflObject.php
@@ -1,17 +1,20 @@
 <?php
+
+namespace Invoker;
+
 class ReflObject {
         public $obj = null;
 
         public function run($method, $args) {
-			    return call_user_func_array(array($this->obj, $method), array($args));
-	    	}
+		return call_user_func_array(array($this->obj, $method), array($args));
+	}
 
         public function __construct($obj = null) {
-          $this->obj = $obj;
+        	$this->obj = $obj;
 
-            if($this->obj === null){
-                throw new Exception('Object must be defined');
-            }
+        	if($this->obj === null) {
+        		throw new Exception('Object must be defined');
+            	}
         }
 
         protected function init(){
