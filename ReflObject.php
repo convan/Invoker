ec7ac29a3932c32ddc57fb8dc84e3c668a945253
convan <51078084+convan@users.noreply.github.com>
2019-06-25 20:13:52 +0300

    ewf
    wef

---
+++ ReflObject.php
@@ -0,0 +1,24 @@
+<?php
+class ReflObject {
+        public $obj = null;
+
+        public function run($method, $args) {
+			    return call_user_func_array(array($this->obj, $method), array($args));
+	    	}
+
+        public function __construct($obj = null) {
+          $this->obj = $obj;
+
+            if($this->obj === null){
+                throw new Exception('Object must be defined');
+            }
+        }
+
+        protected function init(){
+        	return;// $this->run();
+        }
+
+        public function toString(){
+                return;// $$this->obj;
+        }
+}
